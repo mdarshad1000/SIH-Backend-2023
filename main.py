11c7f1c106c605130b51ebd551bf7a5dac829eb5
Mohammad Arshad <mdarshad1000@gmail.com>
2023-09-07 03:50:13 +0530

    Basic working prototype

---
+++ main.py
@@ -0,0 +1,90 @@
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent
+from langchain import OpenAI, LLMChain
+from langchain.tools import HumanInputRun
+from utility import CustomOutputParser, CustomPromptTemplate, search, annotate, generate
+from langchain.memory import ConversationBufferWindowMemory
+import os
+from dotenv import load_dotenv
+load_dotenv
+# Set up the base template
+template_with_history = """You are a Legal Assistant whose task is to generate a personalised canonical and high quality legal document.
+You need to generate the legal document with the user's detail which is used to address the user's need. Ask targeted questions from the user.
+You have access to the following tools:
+{tools}
+
+Use the following format:
+
+Question: the input question you must understand and eventually generate a legal document for it.
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer is a full fledged legal document and nothing else.
+
+Begin! Remember to only generate Legal Documents which the user has asked. You should ask human for their details which you need while generating the legal document.
+
+Previous conversation history:
+{history}
+
+New question: {input}
+{agent_scratchpad}
+"""    
+
+output_parser = CustomOutputParser()
+
+human_tool = HumanInputRun()
+
+# Define which tools the agent can use to answer user queries
+
+tools = [
+    Tool(
+        name = "Search",
+        func=search,
+        description="useful for when you need to look for the name of legal documents based on the user input."
+    ),
+    Tool(
+        name = "Ask Human",
+        func = human_tool.run,
+        description = "useful for when you have obtained the template of the legal document and want to fill the unknown values so take input from human"
+    ),
+    Tool(
+        name = "Get Template",
+        func = generate,
+        description = "useful for when you need to get a legal document template."
+    ),
+    # Tool(
+    #     name='Generate legal document',
+    #     func = generate,
+    #     description="Useful when you have the legal document and want to generate the legal document with the user details"
+    # )
+]
+
+
+prompt_with_history = CustomPromptTemplate(
+    template=template_with_history,
+    tools=tools,
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps", "history"]
+)
+
+
+llm = OpenAI(temperature=0, openai_api_key=os.getenv('OPENAI_API_KEY'))
+llm_chain = LLMChain(llm=llm, prompt=prompt_with_history)
+
+tool_names = [tool.name for tool in tools]
+agent = LLMSingleActionAgent(
+    llm_chain=llm_chain,
+    output_parser=output_parser,
+    stop=["\nObservation:"],
+    allowed_tools=tool_names
+)
+
+
+memory=ConversationBufferWindowMemory(k=2)
+
+agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+
+agent_executor.run("I need to rent a shop?")
---
+++ utility.py
@@ -0,0 +1,116 @@
+from langchain.agents import Tool, AgentOutputParser
+from langchain.prompts import StringPromptTemplate
+from typing import List, Union
+from langchain.schema import AgentAction, AgentFinish, OutputParserException
+from dotenv import load_dotenv
+import openai
+import re
+import os
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Set up a prompt template
+class CustomPromptTemplate(StringPromptTemplate):
+    # The template to use
+    template: str
+    # The list of tools available
+    tools: List[Tool]
+
+    def format(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
+        kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        return self.template.format(**kwargs)
+
+
+class CustomOutputParser(AgentOutputParser):
+
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise OutputParserException(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+    
+
+def search(query):
+    # print("query is", query)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "user",
+        "content": f"You are a very smart Legal Assitant who takes a sentence as input and based on that you generate the exact name of the legal documnet (Power of Attorney, Will Testament, Sale and Purchase agreement etc.) which helps their cause. You follow brevity and your response is very small. Following is the user query:\n{query}"
+        }
+    ],
+    temperature=0,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    # print("Intermediate response", response["choices"][0]["message"]["content"])
+    return response["choices"][0]["message"]["content"]
+
+
+def generate(query):
+    print("query is", query)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "user",
+        "content": f"You are a very smart Legal Assitant who takes the name of a legal document as an input and based on that you generate a full fledged template according to Indian law. Your output is a complete document without any prefixes. Add the sentence  at the end 'Replace all placeholders with user detail'. Following is the user query:\n{query}"
+        }
+    ],
+    temperature=0,
+    max_tokens=3900,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    print("Intermediate response", response["choices"][0]["message"]["content"])
+    return response["choices"][0]["message"]["content"]
+
+def annotate(query):
+    print("query is", query)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "user",
+        "content": f"You are a very smart Legal Assitant who takes the user details and a legal document as an input and based on that you regenerate the template with user details. Your output is correct and you dont manipulate the legal document or the user input. Following is the legal document and user detail:\n{query}"
+        }
+    ],
+    temperature=0,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    print("Intermediate response", response["choices"][0]["message"]["content"])
+    return response["choices"][0]["message"]["content"]
